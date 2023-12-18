cc22bc736af4a39e2e2c61f8525343b84dfdf021
pashka <pavelravic@gmail.com>
2023-12-18 11:52:36 +0300

    add: function clear_label_image

--- graphics.py
+++ graphics.py
@@ -1,18 +1,2 @@
-    # Открыть картинку
-    def open_img(self):
-        try:
-            if self.send_path != '':
-                self.clear_label_image()
-            self.box.delete(0, END)
-            path = filedialog.askopenfilename(title='Открыть', type=".jpg")
-            if path:
-                self.send_path = path
-                img = ImageTk.PhotoImage(Image.open(path).resize((650, 400)))
-                self.panel = Label(image=img)
-                self.panel.image = img
-                self.panel.place(x=620, y=40)
-            else:
-                self.send_path = ''
-                self.box.insert(END, 'ERROR:    Картинка не выбрана')
-        except Exception as ex:
-            self.box.insert(END, 'ERROR:    ' + str(ex))
+ def clear_label_image(self):
+        self.panel.destroy()
