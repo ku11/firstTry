3be64b24c8122f89c30135602dc250eb75cae238
pashka <pavelravic@gmail.com>
2023-12-18 11:53:19 +0300

    add: function display

--- graphics.py
+++ graphics.py
@@ -1,2 +1,8 @@
- def clear_label_image(self):
-        self.panel.destroy()
+def display(self, image):
+        try:
+            self.clear_label_image()
+            self.panel = Label(image=image)
+            self.panel.image = image
+            self.panel.place(x=620, y=40)
+        except Exception as ex:
+            self.box.insert(END, 'ERROR:    ' + str(ex))
