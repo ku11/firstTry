a95dc10009d45897172f595b9ee686bc13683321
Jinnee <sdfd>
2023-12-18 10:41:47 +0300

    write first strings func

--- resize_image.py
+++ resize_image.py
@@ -1,3 +1,8 @@
 import cv2
 from PIL import Image
 import numpy as np
+
+def resize_img(img_cv2):
+    tmp_img = cv2.cvtColor(img_cv2, cv2.IMREAD_COLOR)
+    img = Image.fromarray(tmp_img)
+    width, height = img.size
