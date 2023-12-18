7e50631e5e57f4206e2bdc0474a3132fa2eaaee4
Jinnee <sdfd>
2023-12-18 10:42:21 +0300

    write second strings func

--- resize_image.py
+++ resize_image.py
@@ -6,3 +6,11 @@ def resize_img(img_cv2):
     tmp_img = cv2.cvtColor(img_cv2, cv2.IMREAD_COLOR)
     img = Image.fromarray(tmp_img)
     width, height = img.size
+    white_width = 0
+    white_height = 0
+    while (white_width < width or white_height < height):
+        white_height += 9
+        white_width += 16
+
+    white_img = Image.new('RGB', (white_width, white_height), 'white')
+
