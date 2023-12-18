615ac9ea94319cfff5e6b589ab2da61115ccd480
Rostislaw Pawlowich <rostfreeman1@gmail.com>
2023-12-18 15:50:53 +0300

    TRUE SUCCESS

--- Mid_Serv.py
+++ Mid_Serv.py
@@ -29,25 +29,28 @@ def get_watermark_image(filepath):
     except requests.exceptions.RequestException:
         return "OtherError"
 
-    app = Flask(__name__)
-    app.debug = False
 
-    @app.route('/api/test', methods=['POST'])
-    def test():
-        r = request
-        nparr = np.fromstring(r.data, np.uint8)
-        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        img_resized = resize_img(img)
+app = Flask(__name__)
+app.debug = False
 
-        filename = 'tmp.jpg'
-        path = os.curdir + filename
-        cv2.imwrite(path, img_resized)
 
-        result = get_watermark_image(path)
-        if isinstance(result, (int, str)):
-            return Response(status=result)
-        img2 = cv2.imread(path)
-        _, img_encoded = cv2.imencode('.jpg', img2)
-        return Response(img_encoded.tostring())
+@app.route('/api/test', methods=['POST'])
+def test():
+    r = request
+    nparr = np.fromstring(r.data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img_resized = resize_img(img)
 
-    app.run(host="0.0.0.0", port=3000)
+    filename = 'tmp.jpg'
+    path = os.curdir + filename
+    cv2.imwrite(path, img_resized)
+
+    result = get_watermark_image(path)
+    if isinstance(result, (int, str)):
+        return Response(status=result)
+    img2 = cv2.imread(path)
+    _, img_encoded = cv2.imencode('.jpg', img2)
+    return Response(img_encoded.tostring())
+
+
+app.run(host="0.0.0.0", port=3000)
