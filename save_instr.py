df0aa35a3e12d050dce017f06b6ace439929cf95
AlexandPetriko <s.petriko@mail.ru>
2023-12-18 10:42:39 +0300

    second commit save_instr

--- save_instr.py
+++ save_instr.py
@@ -11,6 +11,33 @@ def add_to_origin_table(file_name):
 def add_to_edited_table(file_name):
     add_to_any_table(file_name, "Edited")
 
+def add_to_any_table(file_name, table_name):
+    try:
+        engine = create_engine('sqlite:///Save_image.db', echo=False)
+        conn = engine.connect()
+        meta = MetaData(engine)
+        table = Table(
+            table_name, meta,
+            Column('id', Integer, primary_key=True),
+            Column('filename', String, nullable=False),
+            Column('date_time', String, nullable=False),
+        )
+        meta.create_all(engine)
+        s = table.select()
+        result = conn.execute(s)
+        extension = file_name.find('.')
+        counter = 0
+        for i in result:
+            if i[1] == file_name or i[1] == file_name[:extension] + str(counter) + file_name[extension:]:
+                counter = counter + 1
+        if counter > 0:
+            file_name = file_name[:extension] + str(counter) + file_name[extension:]
+        current_datetime = datetime.now()
+        conn.execute(table.insert(), [
+            {'filename': file_name, 'date_time': str(current_datetime)}])
+    except Exception as e:
+        print("Error!", e.__class__, "occurred.")
+
 
 
 
